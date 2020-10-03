52ee71edc8b5357467e39f862ace69eed4cd2a01
Jahnic <jahnic.beck-joseph@mail.mcgill.ca>
2020-10-03 14:52:56 -0400

    parse tweets and save as csv'

--- test.py
+++ test.py
@@ -17,7 +17,9 @@ from twitter_scraper import get_tweets
 #         file.write(str(tweet.text.encode('utf-8')))
 #     file.close()
 
-
+hashtags = "#maga #trump #donaldtrump #conservative #republican #makeamericagreatagain\
+        #keepamericagreat #america #trumptrain #draintheswamp #americafirst #walkaway\
+        #thegreatawakening #buildthewall #fakenews #trumpsupporters #gop"
 
 # List of hashtags that we're interested in
 keywords = ['#MAGA', '#Trump2020', '#antitrump']
@@ -26,3 +28,64 @@ for tweet in get_tweets('#twitter', pages=1):
     print(tweet['text'])
     
 tweets = get_tweets("#MAGA", 5)
+
+def dict_from_file(f):
+    def line_splitter(line):
+        items = line.strip().split()
+        return items[0], items[1:]
+    # To use filenames, change 'f' above to filename and
+    # use this line:
+    # with open(filename) as f:
+    return {k:v
+            for line in f
+            for k, v in (line_splitter(line),)}
+
+import os
+import json
+import re
+data = []
+directory = '/home/jahnic/Git/Hackathon/TweetScraper/Data/tweet'
+for filename in os.listdir(directory):
+    tweet_file = directory + "/" + filename
+    with open(tweet_file, 'r') as file:
+        tweet_data = file.read()
+     
+    data.append(tweet_data)
+
+# raw_text = []
+# for dat in data:
+#     text = re.search("full_text\": \"[0-9a-zA-Z,.’'@#\s\*]+", dat)
+#     try:
+#         raw_text.append(text.group())
+#     except:
+#         print('='*50)
+#         print('Could not match:', data[140:200])
+#         print('='*50)
+# full_text
+
+raw_text = []
+for dat in data:
+    text = re.search("full_text\": \".*", dat)
+    try:
+        t = text.group()[: 300]
+        raw_text.append(t)
+    except:
+        print('='*50)
+        print('Could not match:')
+        print('='*50)
+raw_text
+
+clean_text = []
+for txt in raw_text:
+    # trim 'full_text": from txt 
+    trimmed = txt[12:]
+    clean = re.search('.*@*\"', trimmed)
+    try:
+        clean_text.append(clean.group())
+    except:
+        print('='*50)
+        print('Could not match:')
+        print('='*50)
+        
+clean_text[8]
+raw_text[8][13:]
